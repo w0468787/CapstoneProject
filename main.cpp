41330202c0dd523d1ff4b6339935fa4089b5b4ef
Drew Villard <w0468787@campus.nscc.ca>
2023-10-03 19:00:45 -0300

    Project Repo Created

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello! Welcome to SecureNetChat, this application provides real time encryption by"
+                 "encrypting user input then sending it across OSI 7 layer model. and decrypting"
+                 "it at the client side of the reciever" << std::endl;
+
+
+
+    //Socket attempts
+
+    //Testing
+
+    //encrypt
+
+
+    //gui
+
+
+    return 0;
+}
